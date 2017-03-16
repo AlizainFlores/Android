335f5f4283d70b544ce62896d0c7016f53e8640a
Alizain Flores <alizainflores22@gmail.com>
2017-03-16 08:01:20 +0800

    Add files via upload

---
+++ MyTabhost/src/com/example/mytabhost/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.mytabhost;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.view.Menu;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+
+@SuppressWarnings("deprecation")
+public class MainActivity extends TabActivity {
+	
+	TabHost th;
+	String lng="", lat="";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        th = this.getTabHost();
+        Intent loclist = new Intent(this, LocationList.class);
+        
+        Intent aboutme = new Intent(this, AboutMe.class);
+        
+        Bundle b = this.getIntent().getExtras();
+		if(b != null)
+		{
+			 lng = b.getString("lng");
+			 lat = b.getString("lat");
+			 loclist.putExtra("lng", lng);
+		     loclist.putExtra("lat", lat);
+		}
+        
+        Drawable add = this.getResources().getDrawable(R.drawable.add);
+        Drawable list = this.getResources().getDrawable(R.drawable.mylocation);
+        Drawable me = this.getResources().getDrawable(R.drawable.share);
+        
+        TabSpec locationlist = th.newTabSpec("list")
+        		.setIndicator("", list)
+        		.setContent(loclist);
+        
+        TabSpec boutme = th.newTabSpec("me")
+        		.setIndicator("", me)
+        		.setContent(aboutme);
+        
+       th.addTab(locationlist);
+       th.addTab(boutme);
+        
+    }
+
+    
+}
